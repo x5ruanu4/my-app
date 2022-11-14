7e6e17425831f4ab7a0dd43f5b780485808a6600
神農尚紀 <x5ruanu4@gmail.com>
2022-11-14 08:02:44 +0000

    Update 6 files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+
+export const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
+      <ColoredMessage color="pink">元気です！</ColoredMessage>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
